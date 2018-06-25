a1f0f5425df1a7a26c59d8cfdeb09554e512ec62
Charlie Schluting <charlie@schluting.com>
2018-06-25 15:37:25 -0500

    refactor, color, and submenus with price/counts

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ## About
 A script (for bitbar) that outputs USD balances of your Ethereum wallets, and all tokens.
 Can be used outside of bitbar - just run directly.
+
+![screenshot](https://schluting.com/temp/bitbar-eth-bal.png "Screenshot")
+
 ## Usage
 Edit the script to input your specific WALLET\_ADDRESSES - the ones provides are random.
 You may also need to update the path to python3 on the first line...
--- eth_wallet_balances.1m.py
+++ eth_wallet_balances.1m.py
@@ -4,7 +4,6 @@
    showing each total value for owned ETH and each token.
 """
 import time
-import operator
 from collections import defaultdict
 
 import requests
@@ -53,7 +52,10 @@ if __name__ == '__main__':
                     token_price = float(token['tokenInfo']['price']['rate'])
                 else:
                     token_price = 0
-                my_tokens[token['tokenInfo']['symbol']] += float(token['balance']) * token_price
+                my_tokens[token['tokenInfo']['symbol']] = defaultdict(float)
+                my_tokens[token['tokenInfo']['symbol']]['price'] = token_price
+                my_tokens[token['tokenInfo']['symbol']]['count'] += float(token['balance'])
+                my_tokens[token['tokenInfo']['symbol']]['usd'] += float(token['balance']) * token_price
         time.sleep(1)
 
     res = requests.get('https://api.coinmarketcap.com/v2/ticker/1027/')
@@ -67,22 +69,31 @@ if __name__ == '__main__':
     my_eth_bal = float(eth_price) * float(eth_sum)
 
     # total $USD amount:
-    total_bal = sum(my_tokens.values()) + my_eth_bal
+    total_bal = sum(x['usd'] for x in my_tokens.values()) + my_eth_bal
     print("%.2f" % total_bal)
     print('---')
-    print("ETH %.2f" % my_eth_bal)
+    print('Balances:')
+    print("ETH %.2f|color=green" % my_eth_bal)
+    print('-- Tokens: %f|color=black' % eth_sum)
+    print('-- Price: %f|color=black' % eth_price)
 
-    for token, bal in sorted(my_tokens.items(), key=operator.itemgetter(1), reverse=True):
-        print(token, "%.2f" % bal)
+    for token in sorted((x for x in my_tokens.items()), key=lambda x: x[1]['usd'], reverse=True):
+        print(token[0], "%.2f|color=green" % token[1]['usd'])
+        print('-- Tokens: %f|color=black' % token[1]['count'])
+        print('-- Price: %f|color=black' % token[1]['price'])
+
     # links to wallet addresses on etherscan.io:
+    print('Wallets:')
     for wallet in WALLET_ADDRESSES:
-        print("Etherscan: %s" % wallet[:6] + "|href=https://etherscan.io/address/" + wallet)
+        print("Etherscan: %s" % wallet[:6] + "|href=https://etherscan.io/address/" + wallet + " color=blue")
 
     res = requests.get('https://api.coinmarketcap.com/v2/ticker/1/')
     res.raise_for_status()
+    if res.json()['data']['symbol'] != 'BTC':
+        raise "coinmarketcap.com API didn't return a BTC price!"
     btc_price = res.json()['data']['quotes']['USD']['price']
+
     # print ETH and BTC prices:
     print("Prices:")
-    print("BTC: %s" % str(btc_price))
-    print("ETH: %s" % str(eth_price))
-
+    print("BTC: %s|color=green" % str(btc_price))
+    print("ETH: %s|color=green" % str(eth_price))
